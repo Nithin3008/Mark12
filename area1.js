141927b68f819b14fcee4769bce7c0b7875208dd
Nithin <60174062+Nithin3008@users.noreply.github.com>
2022-05-29 08:46:07 +0530

    Update area1.js

--- area1.js
+++ area1.js
@@ -1,11 +1,10 @@
-var height1=document.querySelector("#height")
-var base1=document.querySelector("#base")
-var btn1=document.querySelector("#btn")
-var msg1=document.querySelector("#msg")
-function handler()
-{
-    var result=1/2 *(NUmber(height1.value)*Number(base1.value))
-    msg1.innerText="the area of the triange is "+result
+var height1 = document.querySelector("#height");
+var base1 = document.querySelector("#base");
+var btn1 = document.querySelector("#btn");
+var msg1 = document.querySelector("#msg");
+function handler() {
+  var result = (1 / 2) * (Number(height1.value) * Number(base1.value));
+  msg1.innerText = "the area of the triange is " + result;
 }
 
-btn1.addEventListener("click",handler)
+btn1.addEventListener("click", handler);
