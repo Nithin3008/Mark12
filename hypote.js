4ca5b27963a3e81ea55ccb4db1ea660e93c80e50
Nithin <60174062+Nithin3008@users.noreply.github.com>
2022-08-20 11:28:49 +0530

    Added new functionality

--- hypote.js
+++ hypote.js
@@ -1,15 +1,20 @@
-var side1=document.querySelector("#side1")
-var side2=document.querySelector("#side2")
-var msg=document.querySelector("#msg1")
-var btn=document.querySelector("#btn1")
+var side1 = document.querySelector("#side1")
+var side2 = document.querySelector("#side2")
+var msg = document.querySelector("#msg1")
+var btn = document.querySelector("#btn1")
 
-function handler()
-{
-    var a=Number(side1.value)
-    var b=Number(side2.value)
-    var result=Math.sqrt((a*a)+(b*b))
-    console.log(result)
-    msg.innerText="the length of hypotneuse is "+result
+function handler() {
+    var a = Number(side1.value)
+    var b = Number(side2.value)
+    if (a === 0 || b === 0) {
+        msg.innerText = "Enter values properly"
+    }
+    else {
+        var result = Math.sqrt((a * a) + (b * b))
+        console.log(result)
+        msg.innerText = "the length of hypotneuse is " + result
+    }
 }
 
-btn.addEventListener("click",handler)
+
+btn.addEventListener("click", handler)
